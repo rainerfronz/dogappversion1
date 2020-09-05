7abfc31f6c43af2cf5fb5aaa00b99164222eda03
Rainer J Fronz <rainerfronz44@gmail.com>
2020-09-05 16:31:08 -0500

    first coding of dog app
    css, js and html.
    
    submit is not working
    
    need to take entered number and apply it to the fetch request.

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+// 'use strict';
 
+function getDogPictures() {
+  fetch('https://dog.ceo/api/breeds/image/random/3')
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson));
+}
+
+function displayPictures(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="dog-images">`
+  )
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
+function submitForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  submitForm();
+});
