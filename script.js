7f343223b38e3d85eb21b14853525b945621c6db
Rainer J Fronz <rainerfronz44@gmail.com>
2020-09-05 16:45:34 -0500

    part \1

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
 // 'use strict';
 
 function getDogPictures() {
-  fetch('https://dog.ceo/api/breeds/image/random/3')
-    .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson));
-}
-
-function displayPictures(responseJson) {
-  console.log(responseJson);
-  //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="dog-images">`
-  )
-  //display the results section
-  $('.results').removeClass('hidden');
+    fetch('https://dog.ceo/api/breeds/image/random/3')
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson));
 }
 
 function submitForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    getDogImage();
-  });
+    $('form').submit(event => {
+        event.preventDefault();
+        getDogPictures();
+    });
 }
 
-$(function() {
-  console.log('App loaded! Waiting for submit!');
-  submitForm();
+
+
+$(function () {
+    console.log('do it to it!');
+    submitForm();
 });
